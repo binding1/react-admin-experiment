886a94787fb48663715edef0df77859c55d33972
Jaewon Bae <jaey.pro@gmail.com>
2024-01-16 14:54:29 -0500

    add sample list map render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function fetchData() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (res.status === 200) {
+      return await res.json();
+    }
+  } catch (error) {
+    console.log("Fetch data failed:" + error);
+  }
 }
 
-export default App;
+function ListData() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchedData() {
+      const fetched = await fetchData();
+
+      setData(fetched);
+    }
+
+    fetchedData();
+  }, []);
+
+  return data.map((i, index) => {
+    return (
+      <li key={index}>
+        Name: {i.name}
+        <br></br>
+        Username: {i.username}
+      </li>
+    );
+  });
+}
+
+export default function MyApp() {
+  return (
+    <>
+      <h1>React Admin Experiment</h1>
+      <ListData />
+    </>
+  );
+}
